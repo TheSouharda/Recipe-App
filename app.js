011a9cc2d3de9d6f3a469022f8e0492a086f0428
Souharda Biswas <mail2souharda2015@gmail.com>
2021-03-26 12:28:52 +0530

    frontEnd js

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const closeBtn = document.querySelectorAll(".close");
 const popUp = document.querySelectorAll(".popup-view");
 
 var popShow = function (element) {
-  popUp[element].classList;
+  popUp[element].classList.toggle("active");
 };
 
 recipeBtn.forEach((el, i) => {
+  el.addEventListener("click", () => {
+    console.log("hello");
+    popShow(i);
+  });
+});
+
+closeBtn.forEach((el, i) => {
   el.addEventListener("click", () => {
     console.log("hello");
     popShow(i);
